f7bbc66b0b9e439e434638c89a8a519dbf9051b8
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2023-08-13 18:54:01 +0300

    ref: created component Theme.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,4 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { toggleTheme } from 'redux/themeSlice';
-import { selectTheme } from 'redux/selectors';
+import Theme from './Theme/Theme';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
@@ -9,28 +6,10 @@ import { ContactList } from './ContactList/ContactList';
 import css from './App.module.css';
 
 export function App() {
-  const dispatch = useDispatch();
-  const darkTheme = useSelector(selectTheme);
-
-  useEffect(() => {
-    if (darkTheme) {
-      document.body.classList.add('darkTheme');
-      document.body.classList.remove('lightTheme');
-    } else {
-      document.body.classList.remove('darkTheme');
-      document.body.classList.add('lightTheme');
-    }
-  }, [darkTheme]);
-
   return (
     <div className={css.appBox}>
       <main>
-        <button
-          className={css.btnTheme}
-          onClick={() => dispatch(toggleTheme())}
-        >
-          Theme: {darkTheme ? 'Light' : 'Dark'}
-        </button>
+        <Theme />
         <h1>Phonebook</h1>
         <ContactForm />
         <h2>Contacts</h2>
