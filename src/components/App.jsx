026b258522543a2afeba525c1f523599a08bdb00
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2023-08-13 10:31:35 +0300

    add Filter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { toggleTheme } from 'redux/themeSlice';
 import { selectTheme } from 'redux/selectors';
 import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 
 import css from './App.module.css';
@@ -33,6 +34,7 @@ export function App() {
         <h1>Phonebook</h1>
         <ContactForm />
         <h2>Contacts</h2>
+        <Filter />
         <ContactList />
       </main>
     </div>
